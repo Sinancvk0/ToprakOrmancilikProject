1bc73af5c7a3e8bdaf14e8f2443602ae702f765e
sinan <sinancivak00@gmail.com>
2023-11-24 22:21:19 +0300

    Modeller  Oluşturulmaya Başlandı

---
+++ Model/About.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class About : BaseModel
+    {
+        public string Image { get; set; }
+
+        public string Title2 { get; set; }
+
+        public string Description { get; set; }
+        public string Description2 { get; set; }
+
+    }
+}
---
+++ Model/BaseModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class BaseModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool İsActive { get; set; }
+        public bool isDeleted { get; set; } = false;
+        public bool isActive { get; set; } = true;
+
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+
+    }
+}
