25c115cb5e65110f4e1dcad41a9b5d7859b3f9c9
sinan <sinancivak00@gmail.com>
2023-11-27 04:08:41 +0300

    Partial 1

---
+++ ToprakProject/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToprakProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult HeadPartial()
+        {
+
+            return PartialView();
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ ToprakProject/ViewComponents/MainSlider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace ToprakProject.ViewComponents
+{
+    public class MainSlider :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
