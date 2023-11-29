ac8591835eadc04a825622c2d6406c4d20cc3143
sinan <sinancivak00@gmail.com>
2023-11-30 02:12:44 +0300

    Layout Oluşturuldu

--- ToprakProject/Program.cs
+++ ToprakProject/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Default}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
