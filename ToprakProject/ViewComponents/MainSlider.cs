e307b5e143cbe5d0ac4845b9080d9b171c7462b9
sinan <sinancivak00@gmail.com>
2023-11-30 01:29:12 +0300

    new thema

--- ToprakProject/ViewComponents/MainSlider.cs
+++ ToprakProject/ViewComponents/MainSlider.cs
@@ -1,14 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bussiness.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace ToprakProject.ViewComponents
 {
     public class MainSlider :ViewComponent
     {
+        private readonly ISliderService _slider;
+
+        public MainSlider(ISliderService slider)
+        {
+            _slider = slider;
+        }
+
         public IViewComponentResult Invoke()
         {
-
-            return View();
+            var values = _slider.TGetList();
+            return View(values);
         }
     }
 }
