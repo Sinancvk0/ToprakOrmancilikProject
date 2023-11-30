8918065dd236d846b5e6a43dcd5c5866c87adbc3
sinan <sinancivak00@gmail.com>
2023-12-01 00:50:12 +0300

    Componentler eklendi

---
+++ ToprakProject/ViewComponents/Product.cs
@@ -0,0 +1,21 @@
+﻿using Bussiness.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToprakProject.ViewComponents
+{
+    public class Product:ViewComponent
+    {
+        private readonly IProductService _product;
+
+        public Product(IProductService product)
+        {
+            _product = product;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var values=_product.TGetList(); 
+            return View(values);
+        }
+    }
+}
---
+++ ToprakProject/ViewComponents/Slider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToprakProject.ViewComponents
+{
+    public class Slider:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();  
+        }
+    }
+}
