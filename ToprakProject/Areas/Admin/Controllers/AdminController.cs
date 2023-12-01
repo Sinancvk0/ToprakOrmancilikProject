bcf726df3a7b1c3f5db447715d7e2b4b19d63205
sinan <sinancivak00@gmail.com>
2023-12-01 04:55:40 +0300

    Admin paneli oluşturuldu

---
+++ ToprakProject/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToprakProject.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult SidebarPartial()
+        {
+            return PartialView();
+        }
+    }
+}
