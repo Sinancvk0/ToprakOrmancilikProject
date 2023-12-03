35b8539d4281e176f1578aa817d8e00fdb2f003f
sinan <sinancivak00@gmail.com>
2023-12-04 00:27:49 +0300

    Blog ve hizemmtler düzenlendi

---
+++ ToprakProject/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToprakProject.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ToprakProject/ViewComponents/Questions.cs
+++ ToprakProject/ViewComponents/Questions.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bussiness.Services;
+using Data.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ToprakProject.ViewComponents
 {
     public class Questions:ViewComponent
     {
+       private readonly IQuestionService _questionService;
+
+        public Questions(IQuestionService questionService)
+        {
+            _questionService = questionService;
+        }
+
         public IViewComponentResult Invoke()
         {
+            var values = _questionService.TGetList();
 
-            return View();
+            return View(values);
         }
     }
 }
